667860c6ca9ebe0e51f87471742f7f47186d2f2f
dikri3115 <dikri.arrusyd@gmail.com>
2024-06-14 21:51:13 +0700

    update js + index.html

--- js/script.js
+++ js/script.js
@@ -1,1 +1,20 @@
 //ini komen
+
+function konversiSuhu() {
+    var input = document.getElementById("inputSuhu").value;
+    var hasilTextArea = document.getElementById("hasilKonversi");
+    var suhu;
+
+    if (input !== -1) {
+        // Konversi dari Celcius ke Fahrenheit
+        suhu = parseFloat(input);
+        var fahrenheit = (suhu * 9/5) + 32;
+        hasilTextArea.value = fahrenheit.toFixed(1) + "°F";
+    }else {
+        hasilTextArea.value = "Format suhu tidak valid. Harap masukkan suhu dalam format °C atau °F.";
+    }
+}
+function resetForm() {
+    document.getElementById("inputSuhu").value = "";
+    document.getElementById("hasilKonversi").value = "";
+}
