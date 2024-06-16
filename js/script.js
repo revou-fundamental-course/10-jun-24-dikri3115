5a2d19e944d0a41c20692cbb1b492d63a5cbb797
dikri3115 <dikri.arrusyd@gmail.com>
2024-06-16 07:28:24 +0700

    update js + css + index.html

--- js/script.js
+++ js/script.js
@@ -14,8 +14,47 @@ function konversiSuhu() {
         hasilTextArea.value = "Format suhu tidak valid. Harap masukkan suhu dalam format °C atau °F.";
     }
 }
-function resetForm() {
+
+//display
+function switchForm() {
+    let form1 = document.getElementById('Form1');
+    let form2 = document.getElementById('reverseForm2');
+
+    if (form1.style.display == 'none') {
+        form1.style.display = 'block';
+        form2.style.display = 'none';
+    } else {
+        form1.style.display = 'none';
+        form2.style.display = 'block';
+    }
+}
+
+//hasil Fahrenheit
+function konversiSuhuF() {
+    var input = document.getElementById("inputSuhuFahrenheit").value;
+    var hasilhitung = document.getElementById("hasilCelsius");
+    var carakalkulasi = document.getElementById("caraKalkulasiFahr")
+    if (input !== -1) {
+        // Konversi dari Fahrenheit ke Celsius
+        
+        var fahrenheit = (input - 32)*5/9;
+        hasilhitung.value = fahrenheit.toFixed(1) + "°C";
+        carakalkulasi.value = "("  + input + "- 32)x5/9";
+    }else {
+        hasilhitung.value = "Format suhu tidak valid. Harap masukkan suhu dalam format °C atau °F.";
+    }
+}
+
+//reset form celsius
+
+function resetFormC() {
     document.getElementById("inputSuhu").value = "";
     document.getElementById("hasilKonversi").value = "";
     document.getElementById("caraKalkulasi").value = "";
+}
+
+function resetFormF() {
+    document.getElementById("inputSuhuFahrenheit").value = "";
+    document.getElementById("hasilCelsius").value = "";
+    document.getElementById("caraKalkulasiFahr").value = "";
 }
